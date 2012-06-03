4850098a1a32c6745445271f4ce11c906f047c81
Erland Isaksson <erland@isaksson.info>
2012-06-03 12:48:34 +0200

    Removed static main method which aren't used

--- java/java/src/main/java/com/ickstream/common/ickdiscovery/IckDiscovery.java
+++ java/java/src/main/java/com/ickstream/common/ickdiscovery/IckDiscovery.java
@@ -8,7 +8,6 @@ package com.ickstream.common.ickdiscovery;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
-import java.net.*;
 import java.util.*;
 
 public class IckDiscovery implements com.ickstream.protocol.device.MessageSender
@@ -103,69 +102,4 @@ public class IckDiscovery implements com.ickstream.protocol.device.MessageSender
     public void addMessageListener(MessageListener listener) {
         messageListeners.add(listener);
     }
-
-	public static void main(String[] args) throws InterruptedException {
-        final IckDiscovery discovery = new IckDiscovery();
-        discovery.addMessageListener(new MessageListener() {
-            @Override
-            public void onMessage(String deviceId, String message) {
-                System.out.println(deviceId+": "+message);
-                if(!message.startsWith("I am here")) {
-                    try {
-                        Thread.sleep(1000);
-                        discovery.sendMessage(deviceId, "I am here");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        String deviceId = UUID.randomUUID().toString();
-        final Set<String> devices = new HashSet<String>();
-        discovery.addDeviceListener(new DeviceListener() {
-            @Override
-            public void onDeviceAdded(String deviceId, int services) {
-                System.out.println("Got device: "+deviceId);
-                devices.add(deviceId);
-            }
-
-            @Override
-            public void onDeviceUpdated(String deviceId, int services) {
-                System.out.println("Updated device: "+deviceId);
-                devices.add(deviceId);
-            }
-
-            @Override
-            public void onDeviceRemoved(String deviceId) {
-                System.out.println("Removed device: "+deviceId);
-                devices.remove(deviceId);
-            }
-        });
-        discovery.initDiscovery(deviceId, getNetworkAddress(),"Dummy Client",null);
-        discovery.addService(SERVICE_PLAYER);
-        for(int i=0;i<10;i++) {
-            Thread.sleep(10000);
-            for (String device : devices) {
-                discovery.sendMessage(device, "Hello "+device+" are you there ?");
-            }
-        }
-        discovery.endDiscovery();
-	}
-    private static String getNetworkAddress() {
-        try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
-                NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
-                    InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress()) {
-                        return inetAddress.getHostAddress();
-                    }
-                }
-            }
-        } catch (SocketException ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
-
 }
